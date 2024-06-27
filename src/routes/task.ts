fa52d6086f5a768f91c628567b93d7fdcf077a6b
Von Harrison Tadaiesky <vonharrison26@gmail.com>
2024-06-26 22:28:31 -0300

    feat: add route pactch finishid

--- src/routes/task.ts
+++ src/routes/task.ts
@@ -10,8 +10,20 @@ router.get('/', async (req, res) => {
     res.status(200).json(tasks);
 });
 
-router.get('/:id', (req, res) => {
-    res.send(`Tarefa com ID: ${req.params.id}`);
+router.get('/:id', async (req, res) => {
+    const id = Number(req.params.id)
+
+    try{
+        const task = await prisma.task.findUnique({
+            where: {
+                id: id
+            }
+        })
+        res.status(200).json(task)
+    }catch (error) {
+        res.status(500).send('Erro ao buscar task');
+    }
+
 });
 
 router.post('/', async (req, res) => {
@@ -30,4 +42,57 @@ router.post('/', async (req, res) => {
     }
 });
 
+router.put('/:id', async (req, res) => {
+    const id = Number(req.params.id)
+    const { title, description} = req.body;
+
+    try{
+        const task = await prisma.task.update({
+            where: {
+                id: id
+            },
+            data: {
+                title,
+                description,
+                updated_at: new Date()
+            }
+        })
+        res.status(200).json(task)
+    }catch (error) {
+        res.status(500).send('Erro ao atualizar task');
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    const id = Number(req.params.id)
+    try {
+     const deleteTask = await prisma.task.delete({
+         where: {
+             id: id
+         }
+     })
+    }catch (error) {
+        res.status(500).send('Erro ao deletar task');
+    }
+
+})
+
+router.patch('/:id', async (req, res) => {
+    const id = Number(req.params.id)
+
+    try{
+        const task = await prisma.task.update({
+            where: {
+                id: id
+            },
+            data: {
+                completed_at: true
+            }
+        })
+
+        res.status(201).json(task)
+    }catch (error) {
+        res.status(500).send('Erro ao atualizar task');
+    }
+})
 export default router;
